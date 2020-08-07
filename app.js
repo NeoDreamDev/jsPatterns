788ae904b1a0881708758be02bf01e64c446c126
Salaam Tyrek <39743101+NeoDreamDev@users.noreply.github.com>
2020-08-07 15:51:13 -0400

    commit

--- app.js
+++ app.js
@@ -30,53 +30,53 @@
 // console.log(UICtrl.text);
 
 // REVEALING MODULE PATTERN
-const ItemCtrl = (function () {
-  let data = [];
+// const ItemCtrl = (function () {
+//   let data = [];
 
-  function add(item) {
-    data.push(item);
-    console.log("Item Added...");
-  }
+//   function add(item) {
+//     data.push(item);
+//     console.log("Item Added...");
+//   }
 
-  function get(id) {
-    return data.find((item) => {
-      return item.id === id;
-    });
-  }
-  return {
-    add: add,
-    get: get,
-  };
-})();
-ItemCtrl.add({ id: 1, name: "Salaam" });
-ItemCtrl.add({ id: 2, name: "Tyrek" });
+//   function get(id) {
+//     return data.find((item) => {
+//       return item.id === id;
+//     });
+//   }
+//   return {
+//     add: add,
+//     get: get,
+//   };
+// })();
+// ItemCtrl.add({ id: 1, name: "Salaam" });
+// ItemCtrl.add({ id: 2, name: "Tyrek" });
 
-console.log(ItemCtrl.get(1));
+// console.log(ItemCtrl.get(1));
 
 // SINGLETON PATTERN
 // GIVE GLOBAL POINT OF ACCESS IN STEAD OF ENCAPSULATION
 // CANT HAVE MORE THAN ONE INSTANCE
 
-const Singleton = (function () {
-  let instance;
+// const Singleton = (function () {
+//   let instance;
 
-  function createInstance() {
-    const object = new Object({ name: "salaam" });
-    return object;
-  }
-  return {
-    getInstance: function () {
-      if (!instance) {
-        instance = createInstance();
-      }
-      return instance;
-    },
-  };
-})();
+//   function createInstance() {
+//     const object = new Object({ name: "salaam" });
+//     return object;
+//   }
+//   return {
+//     getInstance: function () {
+//       if (!instance) {
+//         instance = createInstance();
+//       }
+//       return instance;
+//     },
+//   };
+// })();
 
-const instanceA = Singleton.getInstance();
-const instanceB = Singleton.getInstance();
-console.log(instanceA === instanceB);
+// const instanceA = Singleton.getInstance();
+// const instanceB = Singleton.getInstance();
+// console.log(instanceA === instanceB);
 
 // FACTORY PATTERN
 //
